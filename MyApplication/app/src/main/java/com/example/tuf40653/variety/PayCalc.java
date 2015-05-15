3476b70a91e541e9a5380758b4f316fd0c3e92a1
Gintautas Zukas <tuf40653@temple.edu>
2015-05-15 10:07:53 -0400

    Polishing up main fonctionality for first activity

--- MyApplication/app/src/main/java/com/example/tuf40653/variety/PayCalc.java
+++ MyApplication/app/src/main/java/com/example/tuf40653/variety/PayCalc.java
@@ -11,13 +11,10 @@ import android.widget.TextView;
 
 public class PayCalc extends ActionBarActivity {
 
-    private double hrsEntry, rateEntry;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_calc);
-        hrsEntry = rateEntry = 0.0;
     }
 
 
@@ -45,7 +42,7 @@ public class PayCalc extends ActionBarActivity {
 
     //Should close the application
     public void onClose(View view) {
-        this.finish();
+        System.exit(0);
     }
 
     //Calls the calculatePay method and sets the text accordingly
@@ -56,10 +53,35 @@ public class PayCalc extends ActionBarActivity {
         double amount = Calc.calculatePay(Double.parseDouble(hours.getText().toString()), Double.parseDouble(rate.getText().toString()));
         double net = amount - (Calc.calculateSIT(amount) + Calc.calculateFIT(amount) + Calc.calculateSS(amount) + Calc.calculateMed(amount));
         resultText.setText("Gross Pay:" + String.valueOf(amount) +
-                            "\nState Income Tax Deduction:" + String.valueOf(Calc.calculateSIT(amount)) +
-                            "\nFederal Income Tax Deduction:" + String.valueOf(Calc.calculateFIT(amount)) +
-                            "\nSocial Security:" + String.valueOf(Calc.calculateSS(amount)) +
-                            "\nMedicare:" + String.valueOf(Calc.calculateMed(amount)) +
-                            "\nNet Pay:" + String.valueOf(net));
+                "\nState Income Tax Deduction:" + String.valueOf(Calc.calculateSIT(amount)) +
+                "\nFederal Income Tax Deduction:" + String.valueOf(Calc.calculateFIT(amount)) +
+                "\nSocial Security:" + String.valueOf(Calc.calculateSS(amount)) +
+                "\nMedicare:" + String.valueOf(Calc.calculateMed(amount)) +
+                "\nNet Pay:" + String.valueOf(net));
     }
+
+    //Will clear the current entries in the EditText fields
+    public void clearClick(View view){
+        EditText hrs = (EditText) findViewById(R.id.Hours);
+        EditText rate = (EditText) findViewById(R.id.Rate);
+        TextView res = (TextView) findViewById(R.id.TextShowResult);
+        hrs.setText(R.string.hours);
+        rate.setText(R.string.rate);
+        res.setText(R.string.result);
+    }
+
+
+    /* THIS BLOCK WASN'T WORKING THE WAY IT WAS INTENDED TO
+    //Will execute certain actions when the hours EditText field is clicked on
+    public void hoursClick(View view){
+        EditText hours = (EditText) findViewById(R.id.Hours);
+        hours.setText(R.string.blank);
+    }
+
+    //WIll execute certain actions when the rate EditText field is clicked on
+    public void rateClick(View view){
+        EditText rate = (EditText) findViewById(R.id.Rate);
+        rate.setText(R.string.blank);
+    }
+    */
 }
