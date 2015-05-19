abcace8c1f87842dcb02d1395341b1cefcd73185
Gintautas Zukas <tuf40653@temple.edu>
2015-05-19 17:07:51 -0400

    Buttons to hide their fields and move other stuff to fill the void

--- MyApplication/app/src/main/java/com/example/tuf40653/variety/HealthClub.java
+++ MyApplication/app/src/main/java/com/example/tuf40653/variety/HealthClub.java
@@ -1,11 +1,24 @@
 package com.example.tuf40653.variety;
 
+import android.app.ActionBar;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class HealthClub extends ActionBarActivity {
@@ -54,9 +67,34 @@ public class HealthClub extends ActionBarActivity {
     //Will shrink the area it's currently in, and if it's already shrunk, expand back to original state
     public void shrinkClick(View view){
         //Select the parent element, and graciously clear it of all the things in it
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.topGroup);
+        Button btn = (Button) findViewById(R.id.shrink1);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.secondGroup);
+        RelativeLayout relativeLayout1 = (RelativeLayout) findViewById(R.id.thirdGroup);
+        RelativeLayout relativeLayout2 = (RelativeLayout) findViewById(R.id.fourthGroup);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fifthGroup);
 
+        int topFra = frameLayout.getTop();      int bottomFra = frameLayout.getBottom();        int heightFra = frameLayout.getHeight();
+        int topRel = relativeLayout.getTop();   int bottomRel = relativeLayout.getBottom();     int heightRel = relativeLayout.getHeight();
+        int topRel1 = relativeLayout1.getTop(); int bottomRel1 = relativeLayout1.getBottom();   int heightRel1 = relativeLayout1.getHeight();
+        int topRel2 = relativeLayout2.getTop(); int bottomRel2 = relativeLayout2.getBottom();   int heightRel2 = relativeLayout2.getHeight();
+        int topLin = linearLayout.getTop();     int bottomLin = linearLayout.getBottom();       int heightLin = linearLayout.getHeight();
 
-
+        if(btn.getText().toString().contains("Shrink")){
+            btn.setText(R.string.expand1);
+            frameLayout.setVisibility(View.INVISIBLE);
+            relativeLayout.setY(topFra);
+            relativeLayout1.setY(heightRel + 25);
+            relativeLayout2.setY(heightRel + heightRel1 + 40);
+            linearLayout.setY(heightRel + heightRel1 + heightRel2 + 55);
+        }else{
+            btn.setText(R.string.shrink1);
+            frameLayout.setVisibility(View.VISIBLE);
+            relativeLayout.setY(topRel);
+            relativeLayout1.setY(topRel1);
+            relativeLayout2.setY(topRel2);
+            linearLayout.setY(topLin);
+        }
     }
 
     //Needs to have a method that will be called whenever something is changed in the form
